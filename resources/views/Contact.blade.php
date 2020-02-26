1c14f9a3fd6cffa09b5b5c1afcbe9ed681754101
AndrewNyairo <andrewnyairo@gmail.com>
2020-02-26 15:17:43 +0300

    My commits

---
+++ resources/views/Contact.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+  <h1>This is Contact page.</h1>
+  @section('content')
+@endsection
---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,5 @@
+<div class="sidebar">
+  <h2>Sidebar</h2>
+  This is the sidebar
+  @show
+</div>
